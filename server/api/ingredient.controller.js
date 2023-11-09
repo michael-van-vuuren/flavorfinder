d052049a1a294337cd413d765655d9a810114053
michael van vuuren <mvanvuuren@g.ucla.edu>
2023-11-09 15:45:55 -0800

    added db to hold ingredients and ability to add new ones

---
+++ server/api/ingredient.controller.js
@@ -0,0 +1,11 @@
+import IngredientDAO from '../dao/ingredientDAO.js'
+
+export default class IngredientController {
+  static async apiGetIngredients(req, res, next) {
+
+  }
+
+  static async apiGetIngredientImage(req, res, next) {
+
+  }
+}
