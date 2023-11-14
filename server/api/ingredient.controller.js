e913d21a5ce81cf072e2af252fec23265ef19d91
michael van vuuren <mvanvuuren@g.ucla.edu>
2023-11-13 16:59:06 -0800

    ingredients are now fetched from server

--- server/api/ingredient.controller.js
+++ server/api/ingredient.controller.js
@@ -1,27 +1,48 @@
-import IngredientDAO from '../dao/ingredientDAO.js'
+import { readFile } from 'fs/promises';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
 
-export default class IngredientController {
-  // res = [{"_id":"..","name":".."},..]
-  static async apiGetIngredientsNoImage(req, res, next) {
-    try {
-      let ingredients = await IngredientDAO.getIngredientsNoImage()
-      res.json(ingredients || [])
-    } catch (e) {
-      console.log(`cannot get the ingredient labels, ${e}`)
-      res.status(500).json({ error: e })
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+export default class IngredientCtrl {
+    static filePath = join(__dirname, '../resources/ingredients.json');
+
+    static async fetchData() {
+        try {
+            const data = await readFile(IngredientCtrl.filePath, 'utf-8');
+            return JSON.parse(data);
+        } catch (error) {
+            console.error('error reading or parsing data:', error);
+            throw error;
+        }
     }
-  }
 
-  // res = "<imageurl>"
-  static async apiGetIngredientImage(req, res, next) {
-    try {
-      const ingredientId = req.params.id
-
-      let image = await IngredientDAO.getIngredientImage(ingredientId)
-      res.json(image)
-    } catch (e) {
-      console.log(`cannot get the ingredient image, ${e}`)
-      res.status(500).json({ error: e })
+    static async serverGetIngredientsNoImage(req, res) {
+        try {
+            const data = await IngredientCtrl.fetchData();
+            const result = data.map(({ _id, name }) => ({ _id, name }));
+            res.json(result);
+        } catch (error) {
+            console.error('unexpected error:', error);
+            res.status(500).json({ error: 'internal server error' });
+        }
     }
-  }
+
+    static async serverGetIngredientImage(req, res) {
+        try {
+            const id = req.params.id;
+            const data = await IngredientCtrl.fetchData();
+            const selectedIngredient = data.find((item) => item._id == id);
+
+            if (selectedIngredient) {
+                res.json(selectedIngredient.image);
+            } else {
+                res.status(404).json({ error: 'ingredient not found' });
+            }
+        } catch (error) {
+            console.error('unexpected error:', error);
+            res.status(500).json({ error: 'internal server error' });
+        }
+    }
 }
