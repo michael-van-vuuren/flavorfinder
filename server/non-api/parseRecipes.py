6b158c0a22d1075d9973df9dabb88c8d8e6005ad
zigzag2002 <lukexie2002@gmail.com>
2023-11-24 20:54:14 -0800

    parsed ingredients from every recipe

--- server/non-api/parseRecipes.py
+++ server/non-api/parseRecipes.py
@@ -2,8 +2,21 @@ import requests
 import json
 from bs4 import BeautifulSoup
 
-links = []
-f = open("../resources/wikipedia-recipe-links.json", "w")
+links = set()
+recipeToIngredients = {}
+
+def parse_recipe_url(link):
+    response = requests.get(
+	    url=link,
+    )
+    soup = BeautifulSoup(response.content, 'html.parser')
+    try:
+        ingredients = soup.find("div", {"class" : "mw-content-ltr"}).ul.find_all("li")
+        ingredientText = list(map(lambda x:x.text, ingredients))
+        recipeToIngredients[link] = ingredientText
+    except:
+        print(link)
+
 def parse_category(link):
     response = requests.get(
 	    url='https://en.wikibooks.org/' + link,
@@ -14,7 +27,7 @@ def parse_category(link):
         subpages = soup.find(id='mw-pages').find_all("div", {"class" : "mw-category-group"}, recursive=True)
         # get all recipes pages in category
         for page in subpages:
-            links.append(page.a['href'])
+            links.add('https://en.wikibooks.org/' + page.a['href'])
 
     # continue recursively parsing for subcategories
     for category in subcategories:
@@ -23,7 +36,14 @@ def parse_category(link):
     
 def main():
     parse_category('wiki/Category:Recipes_by_ingredient') # recursively parse main recipe page
-    f.write(json.dumps(links))
+    f = open("../resources/wikipedia-recipe-links.json", "w")
+    f.write(json.dumps(list(links))) # write all links
+    f.close()
+
+    for link in links:
+        parse_recipe_url(link)
+    f = open("../resources/recipe-to-ingredients.json", "w")
+    f.write(json.dumps(dict(recipeToIngredients))) # write all links
     f.close()
 
 main()
