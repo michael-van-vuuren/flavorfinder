76780f0746f5ca45cdb31c2542454f63b8760b26
RoyalAscot1 <66349492+RoyalAscot1@users.noreply.github.com>
2023-11-24 20:44:02 -0800

    add new user to database on login

--- client/src/Login.js
+++ client/src/Login.js
@@ -19,6 +19,23 @@ function Login() {
     console.log(error);
   };
 
+  const handleGoogleLogin = async (response) => {
+
+    try {
+      const url = `http://localhost:3001/api/v1/users/new`
+      const res = await fetch(url, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ 
+          user: response.credential
+        })
+      })
+      console.log(res)
+    } catch (e) {
+      console.error('Error adding new user:', e.message)
+    }
+  }
+
   if (login) {
     return (
       <App />
@@ -29,7 +46,7 @@ function Login() {
       <div>
         <h1 style={{ display: "flex", justifyContent: "center", marginTop: "1%" }}>FlavorFinder</h1>
         <div style={{ display: "flex", justifyContent: "center", marginTop: "10%", borderStyle: "solid", marginLeft: "10%", marginRight: "10%", padding: "10%", borderColor: "#d4d4d4", borderRadius: "10px" }}>
-          <GoogleLogin onSuccess={(res) => { setLogin(true); responseMessage(res); }} onError={errorMessage} />
+          <GoogleLogin onSuccess={(res) => { setLogin(true); handleGoogleLogin(res); }} onError={errorMessage} />
         </div>
 
       </div>
