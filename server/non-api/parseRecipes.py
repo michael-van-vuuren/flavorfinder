84688e4a4aafc4277cbe880036d62b99f7f566e8
zigzag2002 <lukexie2002@gmail.com>
2023-11-24 22:09:51 -0800

    parsed all recipe links
    saved a mapping of recipe link to ingredient array in recipe-to-ingredients.json

--- server/non-api/parseRecipes.py
+++ server/non-api/parseRecipes.py
@@ -4,6 +4,11 @@ from bs4 import BeautifulSoup
 
 links = set()
 recipeToIngredients = {}
+unicode = {"\u00bd": "0.5", "\u00bc": "0.25", "\u2154": 2/3, "\u00e9":"e", "\u00a0": " ","\u00f3":"o"
+,"\u00be": "0.75", "\u2153":1/3, "\u00b0":" degrees", "\u215c":"0.375", "\u2014": " ", "\u215b":"0.125",
+"\u00f1": "n", "\u00e8":"e", "\u00ee":"i", "\u00fa":"u", "\u00f4": "o", "\u00e7":"c", "\u201d": " inch"
+,"\u00fb": "u", "\u2159":1/6, "\u215d":0.625, "\u00e4":"a", "\u00ea":"e", "\u00ba":" degrees", "\u00d7":"x", "\u01b0":"u",
+"\u1ebf": "e", "\u1edb":"o", "\u1eaf":"a", "\u00ed":"i", "\u2155":"0.2", "\u2013": "|"}
 
 def parse_recipe_url(link):
     response = requests.get(
@@ -12,7 +17,12 @@ def parse_recipe_url(link):
     soup = BeautifulSoup(response.content, 'html.parser')
     try:
         ingredients = soup.find("div", {"class" : "mw-content-ltr"}).ul.find_all("li")
-        ingredientText = list(map(lambda x:x.text, ingredients))
+        ingredientText = []
+        for ingredient in ingredients:
+            plaintext = ingredient.text
+            for key, value in unicode.items():
+                plaintext = plaintext.replace(key, str(value))
+            ingredientText.append(plaintext)
         recipeToIngredients[link] = ingredientText
     except:
         print(link)
