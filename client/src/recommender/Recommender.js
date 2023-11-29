b93f9aa19c9e2b403185694016ec474fa7e1b7b9
Gautam Mahes <gmahes7@gmail.com>
2023-11-29 15:43:55 -0800

    chatbox for recipebot in progress

--- client/src/recommender/Recommender.js
+++ client/src/recommender/Recommender.js
@@ -1,12 +1,31 @@
-import React from 'react';
+import e from 'cors';
+import React, { useState } from 'react';
 import "./Recommender.css";
 
 const RecipeRecommender = () => {
+  const [userInput, setUserInput] = useState("");
+  const [shownUserText, setShownUserText] = useState("");
+
+
+  const handleChatButtonSubmit = () => {
+    setShownUserText(userInput);
+
+  }
   return (
-    <div>
-      <h2 id="recommender-title">Get cooking</h2>
-        
+    <div style={{ display: "flex", justifyContent: "center", alignContent: "center" }}>
+      <div className='chatBox' >
+
+        {shownUserText}
+        <div className="inputDiv">
+          <input type="text" placeholder="Message RecipeBot..." style={{ width: "500px", }} onChange={(e) => setUserInput(e.target.value)} />
+          <button onClick={handleChatButtonSubmit} style={{ backgroundColor: "green" }}></button>
+        </div>
+
+
+
+      </div >
     </div>
+
   );
 }
 
