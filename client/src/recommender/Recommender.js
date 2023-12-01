25d2f5ad8b48a8b55c8b1cf2d8d789aa2e98d514
Gautam Mahes <gmahes7@gmail.com>
2023-11-30 16:21:39 -0800

    messages show up in the chatbox

--- client/src/recommender/Recommender.js
+++ client/src/recommender/Recommender.js
@@ -4,26 +4,56 @@ import "./Recommender.css";
 
 const RecipeRecommender = () => {
   const [userInput, setUserInput] = useState("");
-  const [shownUserText, setShownUserText] = useState("");
+  const [messages, setMessages] = useState([])
+
 
 
   const handleChatButtonSubmit = () => {
-    setShownUserText(userInput);
+    const userMessage = pushMessage(userInput);
+    setMessages([...messages, userMessage]); // display the message
+    setUserInput(""); // reset input field
+  }
+
+  const handleClearLog = () => {
+    setMessages([]);
+  }
+
+
+  const pushMessage = (message) => {
+    return (
+      <div>
+        <div className='indivdualMessage'>
+          <p>
+            Me:
+          </p>
+          <p>{message}</p>
+
+        </div>
+        <div className='indivdualMessage'>
+          <p>RecipeBot:</p>
+          <p>response</p>
+        </div>
+      </div>
+    )
 
   }
   return (
-    <div style={{ display: "flex", justifyContent: "center", alignContent: "center" }}>
-      <div className='chatBox' >
+    <div>
+      <div>
+        <button onClick={handleClearLog}>Clear Messages</button>
+      </div>
+      <div style={{ display: "flex", justifyContent: "center", alignContent: "center" }}>
+        <div className='chatBox' >
+          <div className='messages'>
+            {messages.map((element) => { return (<div style={{ marginBottom: "10px" }}>{element}</div>) })}
+          </div>
 
-        {shownUserText}
-        <div className="inputDiv">
-          <input type="text" placeholder="Message RecipeBot..." style={{ width: "500px", }} onChange={(e) => setUserInput(e.target.value)} />
-          <button onClick={handleChatButtonSubmit} style={{ backgroundColor: "green" }}></button>
-        </div>
-
-
-
-      </div >
+          <div className="inputDiv">
+            <input type="text" value={userInput} placeholder="Chat with RecipeBot..." style={{ width: "500px", }} onChange={(e) => setUserInput(e.target.value)} />
+            <button onClick={handleChatButtonSubmit} style={{ backgroundColor: "green" }}></button>
+          </div>
+        </div >
+      </div>
     </div>
 
   );
