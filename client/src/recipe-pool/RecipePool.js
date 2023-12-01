596370c196d15720db9d5f414e77b5e66866f9be
Gautam Mahes <gmahes7@gmail.com>
2023-12-01 10:51:21 -0800

    added tolerance slider to recipes page

--- client/src/recipe-pool/RecipePool.js
+++ client/src/recipe-pool/RecipePool.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Slider from '@mui/material-next/Slider';
 import "./RecipePool.css";
 
 const RecipePool = () => {
+  const [sliderValue, setSliderValue] = useState(0)
 
   const RecipeCard = () => {
     return (
@@ -11,10 +13,28 @@ const RecipePool = () => {
     )
   }
 
+  const handleChange = (e) => {
+    setSliderValue(e.target.value)
+  }
+
   return (
     <div>
       <h2 id="recipepool-title">Potential Recipes</h2>
       <p>Based on ingredients in your pantry</p>
+      <div className='sliderContainer'>
+        <p>Tolerance</p>
+        <Slider
+          value={sliderValue}
+          min={0}
+          step={.05}
+          max={1}
+          onChange={handleChange}
+          valueLabelDisplay="auto"
+          aria-labelledby="non-linear-slider"
+          style={{ width: "500%" }}
+        />
+      </div>
+
       <div className="recipeCardContainer">
         {RecipeCard()}
         {RecipeCard()}
