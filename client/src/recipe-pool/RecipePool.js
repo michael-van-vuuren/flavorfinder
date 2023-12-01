014523f02e8b6d9edfb5cef7c8d0d20d1c8df405
Gautam Mahes <gmahes7@gmail.com>
2023-11-30 17:50:48 -0800

    added initial recipe cards to recipe page

--- client/src/recipe-pool/RecipePool.js
+++ client/src/recipe-pool/RecipePool.js
@@ -2,10 +2,27 @@ import React from 'react';
 import "./RecipePool.css";
 
 const RecipePool = () => {
+
+  const RecipeCard = () => {
+    return (
+      <div className='recipeCard'>
+        Recipe
+      </div>
+    )
+  }
+
   return (
     <div>
       <h2 id="recipepool-title">Potential Recipes</h2>
       <p>Based on ingredients in your pantry</p>
+      <div className="recipeCardContainer">
+        {RecipeCard()}
+        {RecipeCard()}
+        {RecipeCard()}
+        {RecipeCard()}
+        {RecipeCard()}
+        {RecipeCard()}
+      </div>
     </div>
   );
 }
