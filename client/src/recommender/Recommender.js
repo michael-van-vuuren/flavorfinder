3255590cb97e1706c9a1ff3b6f33feae3114869e
Ruben De La Torre <rubenbdlt@g.ucla.edu>
2023-12-02 12:15:00 -0800

    updated RecipeBot styling and refactoring

--- client/src/recommender/Recommender.js
+++ client/src/recommender/Recommender.js
@@ -1,63 +1,37 @@
 import React, { useState, useEffect, useRef } from 'react';
+import ChatInput from "./ChatInput.js"
+import ChatMessageBox from "./ChatMessageBox"
 import "./Recommender.css";
 
 const RecipeRecommender = () => {
-  const [userInput, setUserInput] = useState("");
   const [messages, setMessages] = useState([])
   const messagesRef = useRef(null);
 
   useEffect(() => {
-    // Scroll to the bottom of the messages when messages are updated
     if (messagesRef.current) {
       messagesRef.current.scrollTop = messagesRef.current.scrollHeight;
     }
   }, [messages]);
 
-
-  const handleChatButtonSubmit = () => {
-    if (userInput.length !== 0) {
-      const userMessage = pushMessage(userInput);
-      setMessages([...messages, userMessage]); // display the message
-      setUserInput(""); // reset input field
-    }
+  const handleChatButtonSubmit = (message) => {
+    const userMessage = pushMessage(message);
+    setMessages([...messages, userMessage]);
   }
 
   const handleClearLog = () => {
     setMessages([]);
   }
 
-
   const pushMessage = (message) => {
     return (
-      <div>
-        <div className="chat-message">
-            <div className="center-chat-message user">
-              <div className="avatar">
-                <img className="avatar-image" src={"/images/svg/user-avatar.svg"}/>
-              </div>
-              <div className="bot-response">
-                <p>{message}</p>
-              </div>
-            </div>
-          </div>
-          <div className="chat-message">
-            <div className="center-chat-message recipeBot">
-              <div className="avatar">
-              <img className="avatar-image" src={"/images/svg/bot-avatar.svg"}/>
-              </div>
-              <div className="bot-response">
-                <p>response</p>
-              </div>
-            </div>
-          </div>
-      </div>
+      <ChatMessageBox userMessage={message}/> 
     )
   }
 
   return (
     <div>
       <div>
-        <button className="clearButton" onClick={handleClearLog}>Clear Messages</button>
+        <button className="clearButton" style={{marginBottom: "20px"}} onClick={handleClearLog}>Clear Messages</button>
       </div>
       <div className='chatBox'>
         <div className='chatBox chatLog' ref={messagesRef}>
@@ -67,10 +41,7 @@ const RecipeRecommender = () => {
         </div >
       </div>
       <div className="inputDiv">
-        <input className="textInputBox" type="text" value={userInput} placeholder="Chat with RecipeBot..." style={{ width: "625px", height: "36px" }} onChange={(e) => setUserInput(e.target.value)} />
-        <div className="buttonDiv">
-          <button className="submitButton" onClick={handleChatButtonSubmit}>Send</button>
-        </div>
+        <ChatInput onSubmit={handleChatButtonSubmit}/>
       </div>
     </div>
 
