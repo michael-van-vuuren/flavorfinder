fae033224786a159d063d879a671b2393b5f6367
michael van vuuren <mvanvuuren@g.ucla.edu>
2023-12-04 16:35:33 -0800

    better ui in Recipes and RecipeBot pages

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,18 +1,23 @@
+import React, { useState } from "react";
 import "./App.css";
-import Typewriter from 'typewriter-effect';
-import Accordion from '@mui/material/Accordion';
-import AccordionSummary from '@mui/material/AccordionSummary';
-import Typography from '@mui/material/Typography';
-import AccordionDetails from '@mui/material/AccordionDetails';
+import Typewriter from "typewriter-effect";
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import Typography from "@mui/material/Typography";
+import AccordionDetails from "@mui/material/AccordionDetails";
 
-function Home({ returningUser, name }) {
+function Home({ returningUser, name, pantrySize }) {
     const welcomeStrings = returningUser
         ? [`Welcome back ${name}!`]
         : [`Welcome, ${name}!`];
 
-    const tutorialStrings = returningUser
-        ? ['Visit the Pantry tab to add ingredients']
-        : ['Visit the Pantry tab to get started'];
+    const welcomeParagraph = `You have ${pantrySize} items in your pantry`
+
+    const [expandedAccordion, setExpandedAccordion] = useState(null);
+
+    const handleChangeAccordion = (panel) => (event, isExpanded) => {
+        setExpandedAccordion(isExpanded ? panel : null);
+    };
 
     return (
         <div className="content active-content">
@@ -24,70 +29,106 @@ function Home({ returningUser, name }) {
                         loop: false,
                         delay: 75,
                         deleteSpeed: Infinity,
-                        cursor: '',
+                        cursor: "",
                     }}
                 />
             </h2>
             <div>
-                <p>
-                </p>
-                <p>
-                    This is your personal recipe assistant! To get started, click the dropdowns to learn more!
-                </p>
+                <p></p>
+                <p>{welcomeParagraph}</p>
                 <br></br>
                 <div className="accordionContainer">
-                    <Accordion>
+                    <Accordion
+                        expanded={expandedAccordion === "panel1"}
+                        onChange={handleChangeAccordion("panel1")}
+                    >
                         <AccordionSummary>
-                            <Typography variant="h6">Stock your Pantry!</Typography>
+                            <Typography variant="h6">Stock your Pantry</Typography>
                         </AccordionSummary>
                         <AccordionDetails>
                             <Typography>
-                                Start by adding ingredients in the <b>Pantry</b> tab.
+                                Start by adding ingredients in the <b>Pantry</b> tab
                             </Typography>
                             <ul>
-                                <li className="listElement">Click <b>Add Ingredients</b> at the bottom of the Pantry tab.</li>
-                                <li className="listElement">Enter ingredient details and press <b>Add!</b></li>
-                                <li className="listElement">Confirm your selections at the bottom of the page when done.</li>
-                                <li className="listElement">To remove an ingredient, use the <b>Remove</b> button.</li>
+                                <li className="listElement">
+                                    Click <b>Add Ingredients</b> at the bottom of the Pantry tab
+                                </li>
+                                <li className="listElement">
+                                    Enter ingredient details and press <b>Add</b>
+                                </li>
+                                <li className="listElement">
+                                    Confirm your selections at the bottom of the page when done
+                                </li>
+                                <li className="listElement">
+                                    To remove an ingredient, use the <b>Remove</b> button
+                                </li>
                             </ul>
                         </AccordionDetails>
                     </Accordion>
-                    <Accordion>
+                    <Accordion
+                        expanded={expandedAccordion === "panel2"}
+                        onChange={handleChangeAccordion("panel2")}
+                    >
                         <AccordionSummary>
-                            <Typography variant="h6">View your Recipes!</Typography>
+                            <Typography variant="h6">View your Recipes</Typography>
                         </AccordionSummary>
                         <AccordionDetails>
                             <Typography>
-                                Once you are done adding ingredients, please enter the <b>Recipes</b> tab!
+                                Once you are done adding ingredients, please enter the{" "}
+                                <b>Recipes</b> tab
                             </Typography>
                             <ul>
-                                <li className="listElement">The Tolerance slider allows you to be more lenient with the available ingredients.</li>
-                                <li className="listElement">A higher tolerance translates to more ingredients needed.</li>
-                                <li className="listElement">Click on a recipe to view the details!</li>
+                                <li className="listElement">
+                                    The Tolerance slider allows you to be more lenient with the
+                                    available ingredients
+                                </li>
+                                <li className="listElement">
+                                    A higher tolerance translates to more ingredients needed
+                                </li>
+                                <li className="listElement">
+                                    Click on a recipe to view the details
+                                </li>
                             </ul>
                         </AccordionDetails>
                     </Accordion>
-                    <Accordion>
+                    <Accordion
+                        expanded={expandedAccordion === "panel3"}
+                        onChange={handleChangeAccordion("panel3")}
+                    >
                         <AccordionSummary>
-                            <Typography variant="h6">Chat with your personal RecipeBot!</Typography>
+                            <Typography variant="h6">
+                                Chat with your personal RecipeBot
+                            </Typography>
                         </AccordionSummary>
                         <AccordionDetails>
                             <Typography>
-                                Feeling overwhelmed? Visit the <b>RecipeBot</b> tab!
+                                Feeling overwhelmed? Visit the <b>RecipeBot</b> tab
                             </Typography>
                             <ul>
-                                <li className="listElement">The RecipeBot will output a specific recipe based on your personal preference.</li>
-                                <li className="listElement">For example, if you like something hot for a cold day, please input that and the RecipeBot will output an appropiate recipe!</li>
+                                <li className="listElement">
+                                    The RecipeBot will output a specific recipe based on your
+                                    personal preference
+                                    <li className="listElement">
+                                        "Choose a recipe for a hot day"
+                                    </li>
+                                    <li className="listElement">
+                                        "Pick something that is sweet and salty"
+                                    </li>
+                                    <li className="listElement">
+                                        "Surpise me"
+                                    </li>
+                                    <li className="listElement">
+                                        "I'm Feeling Lucky"
+                                    </li>
+                                </li>
+
                             </ul>
                         </AccordionDetails>
                     </Accordion>
                 </div>
-
             </div>
-
-
         </div>
-    )
+    );
 }
 
 export default Home;
